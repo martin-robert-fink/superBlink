fccc9c98e4b84d02b9bec354bd540c9c7ebb6369
Martin Fink <mfink+developer@mmfink.com>
2018-11-05 19:47:21 -0800

    Initial Commit

---
+++ src/superBlink.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "superBlink.h"
+
+int main()
+{
+    int error = 0;
+    int ledNum = 0;
+    int colors[NUM_LEDS] = {GREEN_LED, RED_LED, BLUE_LED};
+
+    setupGPIO();
+
+    while(!error) {
+        setLED(colors[ledNum], ON);
+        delay(DELAY);
+        error = setLED(colors[ledNum++], OFF);
+        delay(DELAY);
+        if (ledNum >= NUM_LEDS)
+            ledNum = 0;
+    }
+}
---
+++ src/superBlink.h
@@ -0,0 +1,12 @@
+#define DELAY       200
+#define ON          0x01
+#define OFF         0x00
+#define NUM_LEDS    0x03
+
+#define RED_LED     0x400000
+#define BLUE_LED    0x200000
+#define GREEN_LED   0x080000
+
+void setupGPIO();
+int setLED(int color, int state);
+void delay(int milliseconds);
